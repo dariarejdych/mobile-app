eff911d264ab1953db2fa25d978581ac92f6d90b
Daria <rejdychdaria@gmail.com>
2017-04-22 10:32:10 +0200

    id fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,32 +9,53 @@ import { Geolocation } from 'ionic-native';
     providers: [Weather]
 })
 export class HomePage {
-public weather: any;
-private completedLoadWeather = false;
+    public weather: any;
+    private completedLoadWeather = false;
     constructor(public navCtrl: NavController, public weatherService: Weather) {
-      this.loadWeather();
+        this.loadWeather();
     }
     loadWeather() {
-          this.completedLoadWeather = false;
+        this.completedLoadWeather = false;
         Geolocation.getCurrentPosition().then((resp) => {
             console.log(resp.coords.latitude)
             console.log(resp.coords.longitude)
             this.weatherService.loadWeather(resp.coords.latitude, resp.coords.longitude).then(data => {
-                    this.weather = data;
-                    console.log(this.weather.latitude);
-                    this.completedLoadWeather = true;
-                    this.printTemperature();
-              })
+                this.weather = data;
+                console.log(this.weather.latitude);
+                this.completedLoadWeather = true;
+                this.printTemperature();
+                this.printWindSpeed();
+            })
         }).catch((error) => {
-              console.log('Error getting location', error);
+            console.log('Error getting location', error);
         });
-      }
-        printTemperature() {
-                if (this.completedLoadWeather) {
-                      document.getElementById('temp').textContent = "temperature: " + this.weather.currently.temperature;
-                }
-
-                console.log(this.weather.currently.temperature);
-
-          }
     }
+    printTemperature() {
+        if (this.completedLoadWeather) {
+            document.getElementById('temp').textContent = "temperature: " + this.weather.currently.temperature;
+        }
+
+        console.log(this.weather.currently.temperature);
+
+    }
+    printWindSpeed() {
+        if (this.completedLoadWeather) {
+            document.getElementById('wind-speed').textContent = "wind speed " + this.weather.currently.windSpeed;
+        }
+        console.log(this.weather.currently.windSpeed);
+    }
+
+    printCloudCover() {
+        if (this.completedLoadWeather) {
+            document.getElementById('cloud-cover').textContent = "cloud cover " + this.weather.currently.cloudCover;
+        }
+        console.log(this.weather.currently.cloudCover);
+    }
+    printPressure () {
+      if (this.completedLoadWeather) {
+        document.getElementById('pressure').textContent = "pressure " + this.weather.currently.pressure;
+      }
+      console.log(this.weather.currently.pressure);
+    }
+
+  };
